fc843461d5eeb0977d5898c4bdd313fe5ec8e283
Navina K <navinak321@gmail.com>
2024-01-02 23:16:34 +0530

    changes in ratings

--- movie-search-app/src/Components/Card.jsx
+++ movie-search-app/src/Components/Card.jsx
@@ -9,7 +9,7 @@ function Card({data,openDetail}) {
             <div className='movieDetails'>
                 <div className='box'>
                     <h4 className='title'>{data.title}</h4>
-                    <p className='ratings'>{data.vote_average}</p>
+                    <p className='ratings' style={{color:"black"}}>{data.vote_average > 0?data.vote_average.toFixed(1):data.vote_average}</p>
                 </div>
             </div>
         </div>

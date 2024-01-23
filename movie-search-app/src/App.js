76269211eec63f83c41057b04c33aafb45bd0f06
Navina K <navinak321@gmail.com>
2024-01-23 21:58:39 +0530

    routes updated

--- movie-search-app/src/App.js
+++ movie-search-app/src/App.js
@@ -1,17 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <Router>
-    <Switch>
-      <Route path="/" component={<Header/>} />
-      <Route path="*" element={<h1>Page not found</h1>} />
-    </Switch>
-  </Router>
+      <Routes>
+        <Route path="/" component={<Header/>} />
+        <Route path="*" element={<h1>Page not found</h1>} />
+      </Routes>
+    </Router>
   );
 }
 

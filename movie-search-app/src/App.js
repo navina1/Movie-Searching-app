93d71edc51bfceee069e2a95a531811cf657a117
Navina K <navinak321@gmail.com>
2024-01-23 21:46:21 +0530

    Routes added

--- movie-search-app/src/App.js
+++ movie-search-app/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-      <Header/>
+    <Router>
+    <Switch>
+      <Route path="/" component={<Header/>} />
+    </Switch>
+  </Router>
   );
 }
 

be72e1b1265e4f776fd435e7e0c0909d51de252d
Navina K <navinak321@gmail.com>
2024-01-25 01:50:48 +0530

    discription component styles changed

--- movie-search-app/src/Components/Detail.jsx
+++ movie-search-app/src/Components/Detail.jsx
@@ -4,21 +4,26 @@ function Detail({ selectedMovie }) {
     let img_path = "https://image.tmdb.org/t/p/w500";
     return (
         <div className='detail'>
-            <h1>{selectedMovie.original_title}<a href={selectedMovie.homepage}/></h1>
+            <h1>{selectedMovie.original_title}<a href={selectedMovie.homepage} /></h1>
             <div className='discription'>
-                <div>
-                    <img src={img_path + selectedMovie.poster_path} className='poster' />
+                <div className='poster-container'>
+                    <img className="poster" src={img_path + selectedMovie.poster_path} className='poster' />
                 </div>
-                <div>
-                    <p>{selectedMovie.overview}</p>
+                <div className='content-center'>
+                    <div className='overview-container'>
+                        <p>{selectedMovie.overview}</p>
+                    </div>
                     <div className='details'>
-                        <span>Release Date: {selectedMovie.release_date}</span>
+                        <div className="company">
+                            <span>Release Date: {selectedMovie.release_date}</span>
+                        </div>
+                        
                         <div className='company'>
                             <span>Production:</span>
                             {selectedMovie?.production_companies?.map((company, index) => (
                                 <>
                                     <img key={index} src={img_path + company.logo_path} className='Production' />
-                                    {/* <span>{company.name}</span> */}
+
                                 </>
                             ))}
                         </div>

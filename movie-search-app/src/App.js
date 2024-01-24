0229bcfb5ed28e759a655daed4bc85bb7c7a4cab
Navina K <navinak321@gmail.com>
2024-01-24 22:40:10 +0530

    lazy loading added

--- movie-search-app/src/App.js
+++ movie-search-app/src/App.js
@@ -1,17 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Components/Header';
+//import Header from './Components/Header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import { Suspense, lazy } from 'react';
+const Header = lazy(() => import('./Components/Header'))
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Header/>} />
-        <Route path="*" element={<h1>Page not found</h1>} />
-      </Routes>
-    </Router>
+    <Suspense>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="*" element={<h1>Page not found</h1>} />
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
